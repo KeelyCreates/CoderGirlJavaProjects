12facd2f736be07d98fccb752876970d688868ce
KeelyCreates <46945869+KeelyCreates@users.noreply.github.com>
2019-03-16 15:36:54 -0500

    L49 Adding Machine
    L49 Practice--Adding machine

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+import static java.lang.System.*;
+public class AddingMachine {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(in);
+        double number = 0;
+        double sum = 0;
+        int count = 0;
+        boolean addingNumbers = true;
+
+        out.println("Welcome to Adding Machine!\n" +
+                    "Total = 0");
+        while (addingNumbers == true) {
+            out.print("Add: ");
+            number = keyboard.nextDouble();
+            count++;
+            sum += number;
+            out.println("Total: " +sum);
+            if (number == 0) addingNumbers = false;
+        }
+        out.println("You entered numbers "+count+", for a final total of " +sum);
+    }
+}
